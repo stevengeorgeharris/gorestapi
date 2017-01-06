a4bc9d9d2dd7d812f8c33069e38a8cd2eaa86a7d
stevengeorgeharris <mail@stevengeorgeharris.com>
2017-01-06 18:14:46 +0000

    Updated code styles

--- api.go
+++ api.go
@@ -13,6 +13,7 @@ import (
 type Business struct {
 	ID      string   `json:"id,omitempty"`
 	Name    string   `json:"name,omitempty"`
+	Type    string   `json:"type,omitempty"`
 	Address *Address `json:"address,omitempty"`
 }
 
